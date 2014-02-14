31aee0ce4086d2c26d8690bddb32ba3a83785883
Rudy Perez <rrudy90023@gmail.com>
2014-02-13 22:01:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labus::Application.config.secret_key_base = '64c1d95735b5b861988a868babe54d5414412819307c982abbfe8729389cb933f70f65e2f7653093e56f1b9f60fdfea8042bbecbcd859770619712730390f158'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labus::Application.config.session_store :cookie_store, key: '_labus_session'
