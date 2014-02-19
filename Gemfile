370d94e8794aa30fbb2735208eb44f9cbeeffba4
Rudy Perez <rrudy90023@gmail.com>
2014-02-18 23:27:41 -0800

    Added devise

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass'
 
-
+gem 'devise', '~> 3.2.2'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Labus::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://la-bus-stories.herokuapp.com/' }
+
 end
